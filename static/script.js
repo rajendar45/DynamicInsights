60a02d10887ba6bec1604d9b8df794c3a2b1f454
rajendar45 <rajendar45@gmail.com>
2024-12-12 20:26:05 +0530

    modified data with formats

--- static/script.js
+++ static/script.js
@@ -1,91 +1,48 @@
-// script.js
-
-const executeQueryBtn = document.getElementById('execute-query-btn');
-const displayResultsBtn = document.getElementById('display-results-btn');
-const exportToCsvBtn = document.getElementById('export-to-csv-btn');
-
-const generateSqlForm = document.getElementById('generate-sql-form');
-const executeQueryForm = document.getElementById('execute-query-form');
-const exportToCsvForm = document.getElementById('export-to-csv-form');
-
-const resultsContainer = document.getElementById('results-container');
-
 const generateSqlBtn = document.getElementById('generate-sql-submit-btn');
-const sqlQueryContainer = document.getElementById('sql-query-container');
-const viewChartDataBtn  = document.getElementById('view-chart-data-btn');
-
-
-const refreshBtn = document.createElement('button');
-refreshBtn.textContent = 'Reset';
-refreshBtn.style.marginBottom = '10px';
-
-// Append refresh button to the container
-const container = document.getElementById('container');
-container.appendChild(refreshBtn);
+const displayResultsBtn = document.getElementById('display-results-btn');
+const refreshBtn = document.getElementById('reset-btn');
 
 // Add event listener to refresh button
-refreshBtn.addEventListener('click', () => {
-  // Clear existing content
-  const resultsContainer = document.getElementById('results-container');
-  resultsContainer.innerHTML = '';
+if (refreshBtn) {
+  refreshBtn.addEventListener('click', () => {
+    // Reset query input field
+    const queryInputField = document.getElementById('sql-query-input');
+    queryInputField.value = '';
 
-  // Reset query input field
-  const queryInputField = document.getElementById('sql-query-input');
-  queryInputField.value = '';
-   // Reset view chart data button
-  const viewChartDataBtn = document.getElementById('view-chart-data-btn');
-  viewChartDataBtn.disabled = true;
+    // Refresh the page
+    location.reload();
+  });
+}
 
-  // Hide chart container
-  const chartContainer = document.getElementById('chart-container');
-  chartContainer.style.display = 'none';
-});
+if (generateSqlBtn) {
+  generateSqlBtn.addEventListener('click', () => {
+      console.log('Generate SQL button clicked!');
+      const naturalLanguageInput = document.getElementById('natural-language-input').value;
+      console.log('Natural language input:', naturalLanguageInput);
 
-generateSqlBtn.addEventListener('click', () => {
-    console.log('Generate SQL button clicked!');
-    const naturalLanguageInput = document.getElementById('natural-language-input').value;
-    console.log('Natural language input:', naturalLanguageInput);
-
-
-    fetch('/generate_sql_query', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ natural_language_input: naturalLanguageInput })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })    
-    .then(data => {
-        console.log('Received SQL query:', data.sql_query);
-        document.getElementById('sql-query-input').value = data.sql_query;
-    })
-    .catch(error => console.error('Error:', error));
-    });
-
-/*executeQueryBtn.addEventListener('click', () => {
-    const sqlQuery = document.getElementById('sql-query-input').value;
-    console.log('sqlQuery:', sqlQuery);
-    fetch('/execute_query', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ sql_query: sqlQuery })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-});*/
-
+      fetch('/generate_sql_query', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ natural_language_input: naturalLanguageInput })
+      })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+      })    
+      .then(data => {
+          console.log('Received SQL query:', data.sql_query);
+          document.getElementById('sql-query-input').value = data.sql_query;
+      })
+      .catch(error => console.error('Error:', error));
+  });
+}
 displayResultsBtn.addEventListener('click', async () => {
   const sqlQuery = document.getElementById('sql-query-input').value;
   console.log('sqlQuery:', sqlQuery);
-
   try {
     const response = await fetch('/execute_query', {
       method: 'POST',
@@ -96,7 +53,6 @@ displayResultsBtn.addEventListener('click', async () => {
     });
     const data = await response.json();
     console.log(data);
-
     // Now display the results
     fetch('/display_query_results', {
       method: 'GET',
@@ -106,16 +62,15 @@ displayResultsBtn.addEventListener('click', async () => {
     })
     .then(response => response.json())
     .then(data => {
-      const container = document.getElementById('results-container');
-      container.innerHTML = ''; // Clear existing content
+      const resultsContainer = document.getElementById('results-container');
+      resultsContainer.innerHTML = ''; // Clear existing content
 
       if (data.results.length === 0) {
-        container.innerHTML = '<p>No results available.</p>';
+        resultsContainer.innerHTML = '<p>No results available.</p>';
       } else {
-        // Create table
+        //create table
         const table = document.createElement('table');
         table.style.width = '100%'; // Set table width to 100%
-
         // Create table header row
         const headerRow = document.createElement('tr');
         data.columns.forEach(column => {
@@ -126,17 +81,104 @@ displayResultsBtn.addEventListener('click', async () => {
           headerRow.appendChild(headerCell);
         });
         table.appendChild(headerRow);
-
         // Create table body
         const tableBody = document.createElement('tbody');
         table.appendChild(tableBody);
-
         // Append the table to the results container
-        container.appendChild(table);
-
+        resultsContainer.appendChild(table);
+        //end table
+        //CSV Export logic 
+        const exportButton = document.createElement('button');
+        exportButton.id = 'export-to-csv-btn';
+        exportButton.addEventListener('click', () => {
+          const filename = 'Tempdata';
+          fetch('/export_to_csv', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ filename: filename })
+          })
+          .then(response => response.blob())
+          .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = filename + '.csv';
+            a.click();
+            window.URL.revokeObjectURL(url);
+          })
+          .catch(error => console.error(error));
+        });
+     
+        exportButton.textContent = 'Export to CSV';
+        resultsContainer.appendChild(exportButton);
+        //Penalty display if query contains penalty 
+        if (sqlQuery.toLowerCase().includes('penalty')) {
+          const viewPenaltyDataBtn = document.createElement('button');
+          viewPenaltyDataBtn.id = 'view-chart-data-btn';
+          viewPenaltyDataBtn.textContent = 'View Penalty Data';
+          viewPenaltyDataBtn.addEventListener('click', () => {
+            fetch('/chart-data', {
+              method: 'GET'
+            })
+            .then(response => response.json())
+            .then(data => {
+              const chartDataArray = data.chartData.chartData; // Access the chartData array property
+              //Render the chart using Highcharts
+              Highcharts.chart('bar-chart-container', {
+                chart: {
+                  type: 'column'
+                },
+                title: {
+                  text: 'Penalty Data'
+                },
+                xAxis: {
+                  type: 'category',
+                  categories: chartDataArray.map(function(item) {
+                    return item.x;
+                  })
+                },
+                yAxis: {
+                  title: {
+                    text: 'Penalty'
+                  }
+                },
+                series: [{
+                  name: 'Fein Tax Year',
+                  data: chartDataArray.map(function(item) {
+                    return item.y;
+                  })
+                }]
+              });
+              Highcharts.chart('pie-chart-container', {
+                chart: {
+                  type: 'pie'
+                },
+                title: {
+                  text: 'Penalty Data'
+                },
+                series: [{
+                  name: 'Penalty',
+                  colorByPoint: true,
+                  data: chartDataArray.map(function(item) {
+                    return {
+                      name: item.x,
+                      y: item.y
+                    };
+                  })
+                }]
+              });
+            })
+            .catch(error => console.error('Error fetching penalty data:', error));
+          });
+          resultsContainer.appendChild(viewPenaltyDataBtn);
+        }
+        
         // Function to render table data
         function renderTableData(results) {
           tableBody.innerHTML = '';
+
 
           const currentPageResults = results;
           currentPageResults.forEach(result => {
@@ -150,7 +192,7 @@ displayResultsBtn.addEventListener('click', async () => {
             tableBody.appendChild(row);
           });
         }
-
+        renderTableData(data.results);
         // Function to handle pagination
         function handlePagination() {
           const recordsPerPage = 10;
@@ -193,7 +235,7 @@ displayResultsBtn.addEventListener('click', async () => {
           paginationContainer.appendChild(nextPageBtn);
 
           // Append the pagination container to the results container
-          container.appendChild(paginationContainer);
+          resultsContainer.appendChild(paginationContainer);
         }
 
         handlePagination(); // Call handlePagination() when results are available
@@ -204,93 +246,3 @@ displayResultsBtn.addEventListener('click', async () => {
     console.error(error);
   }
 });
-
-exportToCsvBtn.addEventListener('click', () => {
-  const filename = 'Tempdata';
-  fetch('/export_to_csv', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ filename: filename })
-  })
-  .then(response => response.blob())
-  .then(blob => {
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename + '.csv';
-    a.click();
-    window.URL.revokeObjectURL(url);
-  })
-  .catch(error => console.error(error));
-});
-
-document.getElementById('view-chart-data-btn').addEventListener('click', function() {
-    // Make an AJAX request to retrieve the chart data
-    fetch('/chart-data', {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-      const chartDataArray = data.chartData.chartData; // Access the chartData array property
-      // Render the chart using Highcharts
-      Highcharts.chart('bar-chart-container', {
-        chart: {
-          type: 'column'
-        },
-        title: {
-          text: 'Penalty Data'
-        },
-        xAxis: {
-          type: 'category',
-          categories: chartDataArray.map(function(item) {
-            return item.x;
-          })
-        },
-        yAxis: {
-          title: {
-            text: 'Penalty'
-          }
-        },
-        series: [{
-          name: 'Fein Tax Year',
-          data: chartDataArray.map(function(item) {
-            return item.y;
-          })
-        }]
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-    fetch('/chart-data', {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-      const chartDataArray = data.chartData.chartData; // Access the chartData array property
-      // Render the chart using Highcharts
-      Highcharts.chart('pie-chart-container', {
-        chart: {
-          type: 'pie'
-        },
-        title: {
-          text: 'Penalty Data'
-        },
-        series: [{
-          name: 'Penalty',
-          colorByPoint: true,
-          data: chartDataArray.map(function(item) {
-            return {
-              name: item.x,
-              y: item.y
-            };
-          })
-        }]
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-  });
